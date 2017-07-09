f36b4276b480f89cf54ed697231a5edd6d11a813
Ignacio Chavez <nashio@gmail.com>
2017-07-08 17:41:36 -0700

    new build

--- dist/react-tiny-tabs.js
+++ dist/react-tiny-tabs.js
@@ -70,7 +70,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/Users/yushido/exp/github/tiny-tabs/public";
+/******/ 	__webpack_require__.p = "/Users/ignacio/exp/react-tiny-tabs/public";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 7);
